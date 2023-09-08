4150fe035f1087b6af539cb1fe6676ecfa8e324f
MJ Orton <mjorton22@gmail.com>
2023-09-08 10:23:08 -0600

    adding posts, profile, create post, edit profile views

---
+++ FarmersMarketSocialMedia/BusinessProfileViewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BusinessProfileViewViewController.swift
+//  FarmersMarketSocialMedia
+//
+//  Created by MJ Orton on 9/8/23.
+//
+
+import UIKit
+
+class BusinessProfileViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
