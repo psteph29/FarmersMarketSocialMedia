7ac39858a972ff94c5f5b0403510816738866245
psteph29 <paigestephenson29@gmail.com>
2023-09-15 09:24:25 -0600

    Segues

---
+++ FarmersMarketSocialMedia/Cells/FavoritesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FavoritesCollectionViewCell.swift
+//  FarmersMarketSocialMedia
+//
+//  Created by Paige Stephenson on 9/15/23.
+//
+
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+}
