2d3b361b9e3ca7734fa1190a140260ce9b44e880
kole ervine <koleervine@gmail.com>
2023-09-15 08:52:45 -0600

    funcs added for custom database queries

--- FarmersMarketSocialMedia/Controller/CustomDB-APIController.swift
+++ FarmersMarketSocialMedia/Controller/CustomDB-APIController.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import Firebase
+
+struct FirebaseService {
+    static func fetchFarmersMarkets(zipcodesArray: [Int], completion: @escaping ([BusinessListing]?) -> Void) {
+        let db = Firestore.firestore()
+        db.collection("USDAFarmersMarkets")
+            .whereField("listing_zipcode", in: zipcodesArray)
+            .getDocuments { (snapshot, error) in
+                if let error = error {
+                    print("Error fetching farmers markets: \(error)")
+                    completion(nil)
+                    return
+                }
+                
+                let businessListings = snapshot?.documents.compactMap { document -> BusinessListing? in
+                    if let listingUUID = document.get("listing_uuid") as? String,
+                       let name = document.get("listing_name") as? String,
+                       let address = document.get("listing_address") as? String,
+                       let zipcode = document.get("listing_zipcode") as? Int {
+                        return BusinessListing(listing_uuid: listingUUID, listing_name: name, listing_address: address, listing_zipcode: zipcode)
+                    } else {
+                        print("Failed to parse document \(document.documentID)")
+                    }
+                    return nil
+                } ?? []
+                
+                completion(businessListings)
+        }
+    }
+
+    func createFarmersMarket(businessListing: BusinessListing) {
+        let db = Firestore.firestore()
+        var ref: DocumentReference? = nil
+        
+        var data: [String: Any] = [
+            "listing_name": businessListing.listing_name,
+            "listing_zipcode": businessListing.listing_zipcode,
+            
+            
+            // Add other properties as needed
+        ]
+        
+        ref = db.collection("farmersMarkets").addDocument(data: data) { error in
+            if let error = error {
+                print("Error adding document: \(error)")
+                // Error
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+                // Handle success
+            }
+        }
+        
+        // create uuid and assign it to a string and also to id property in struct
+    }
+    
+    // Insert more here! :^)
+
+}
--- FarmersMarketSocialMedia/Model/BusinessListingStruct.swift
+++ FarmersMarketSocialMedia/Model/BusinessListingStruct.swift
@@ -9,8 +9,17 @@ import Foundation
 
 // Properties will need to be changed to the actual desired names, couldnt remember as of now.
 
-struct BusinessListing: Identifiable {
-    var id: String
-    var name: String
-    var zipcode: String
+struct BusinessListing: Identifiable, Encodable {
+    var id: String { listing_uuid }
+    
+    var listing_USDA_id: Int?
+    var listing_uuid: String
+    var listing_name: String
+    var listing_address: String
+    var listing_zipcode: Int
+    var listing_username: String?
+   // var listing_firestore_uid: not sure the type yet
+    var listing_description: String?
+    var app_generated: Bool?
+   // var listing_image: also not sure how to type this property yet
 }
