0a84f90e763fe42dd458d7db3085d685afe5fbb1
Kole Ervine <koleervine@gmail.com>
2023-10-05 08:16:42 -0600

    added comments to coredatamanager
    added imageview onto post creation screen for func

--- FarmersMarketSocialMedia/Controller/CreatePostsViewController.swift
+++ FarmersMarketSocialMedia/Controller/CreatePostsViewController.swift
@@ -16,6 +16,8 @@ class CreatePostsViewController: UIViewController {
     @IBOutlet weak var postDescriptionTextField: UITextView!
     @IBOutlet weak var leavesBackground: UIImageView!
     
+    @IBOutlet var uploadedImage: UIImageView!
+    
     @IBOutlet weak var backgroundImage: UIImageView!
     
     override func viewDidLoad() {
@@ -26,5 +28,37 @@ class CreatePostsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func postButtonTapped(_ sender: UIButton) {
+        guard let postDescription = postDescriptionTextField.text, !postDescription.isEmpty else {
+            print("Error: Post description is empty!")
+            return
+        }
 
+        let post = Post(
+            id: "",  // Will update after saving to Firestore
+            description: postDescription,
+            date: Date(),
+            imageURL: nil
+        )
+
+        if let postImage = uploadedImage.image {
+            FirebaseService.uploadImageToFirebase(image: postImage) { [weak self] result in
+                switch result {
+                case .success(let imageUrl):
+                    var postWithImage = post
+                    postWithImage.imageURL = imageUrl
+                    if let newID = FirebaseService.createPostForBusinessUser(post: postWithImage) {
+                        // Here you can update the post ID if you want to keep the post object around.
+                    }
+                case .failure(let error):
+                    print("Error uploading image: \(error)")
+                }
+            }
+        } else {
+            if let newID = FirebaseService.createPostForBusinessUser(post: post) {
+                // Update the post ID here if needed.
+            }
+        }
+    }
 }
+
