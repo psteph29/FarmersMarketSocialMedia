d7ec1ed762023fb2a8ad673665a6ea9bd7a7b02b
Kole Ervine <koleervine@gmail.com>
2023-10-08 15:02:57 -0600

    restructed the businesslisting struct and extension

--- FarmersMarketSocialMedia/Model/BusinessListingStruct.swift
+++ FarmersMarketSocialMedia/Model/BusinessListingStruct.swift
@@ -10,8 +10,8 @@ import Foundation
 struct BusinessListing: Identifiable, Encodable {
     var id: String { listing_uuid }
     
-    var listing_profileImageURL: String?
-    var uid: String?
+    var listing_profileImageURL: String? //
+    var uid: String? //
     var listing_USDA_id: Int?
     var listing_uuid: String
     var listing_name: String
@@ -24,13 +24,14 @@ struct BusinessListing: Identifiable, Encodable {
 
 extension BusinessListing {
     init(from favoriteListing: FavoriteBusinessListing) {
+        self.listing_profileImageURL = favoriteListing.listing_profileImageURL ?? "No image available."
         self.uid = favoriteListing.uid ?? "No uid"
+        self.listing_USDA_id = Int(favoriteListing.listing_USDA_id)
+        self.listing_uuid = favoriteListing.listing_uuid!
         self.listing_name = favoriteListing.listing_name!
         self.listing_address = favoriteListing.listing_address!
-        self.listing_description = favoriteListing.listing_description ?? "No description available."
-        self.listing_username = favoriteListing.listing_username ?? "No username."
-        self.listing_uuid = favoriteListing.listing_uuid!
         self.listing_zipcode = Int(favoriteListing.listing_zipcode)
-        self.listing_profileImageURL = favoriteListing.listing_profileImageURL ?? "No image available."
+        self.listing_description = favoriteListing.listing_description ?? "No description available."
+        self.app_generated = favoriteListing.app_generated 
     }
 }
