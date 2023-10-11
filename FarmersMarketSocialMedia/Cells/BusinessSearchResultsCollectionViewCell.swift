29accc7aafcd2142b07497f5ee305584732cca7b
Kole Ervine <koleervine@gmail.com>
2023-10-10 19:09:29 -0600

    moved animation to the cell when the favorite button is tapped instead of the modal

--- FarmersMarketSocialMedia/Cells/BusinessSearchResultsCollectionViewCell.swift
+++ FarmersMarketSocialMedia/Cells/BusinessSearchResultsCollectionViewCell.swift
@@ -21,7 +21,25 @@ class BusinessSearchResultsCollectionViewCell: UICollectionViewCell {
     
     @IBAction func favoriteButtonTapped(_ sender: UIButton) {
         self.onFavorite?()
+        
+        // animation code
+        let totalSegments = 5
+        let itemsPerSegment = 2  // or any other number you wish
+        
+        let shuffledSegments = Array(0..<totalSegments).shuffled()
+        
+        for (segmentIndex, segment) in shuffledSegments.enumerated() {
+            // Shuffle the images and take the first 'itemsPerSegment' items
+            let segmentImages = FruitsAndVeggieImages.shuffled().prefix(itemsPerSegment)
+            
+            for itemIndex in 0..<itemsPerSegment {
+                let delay = Double(segmentIndex * itemsPerSegment + itemIndex) * 0.3
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                    let fruitOrVeggie = spawnFruitOrVeggie(in: self, segment: segment, totalSegments: totalSegments, itemIndex: itemIndex, shuffledImages: Array(segmentImages))
+                    animateFall(of: fruitOrVeggie, in: self)
+                }
+            }
+        }
     }
-    
-
 }
--- FarmersMarketSocialMedia/Controller/UserBusinessProfileViewController.swift
+++ FarmersMarketSocialMedia/Controller/UserBusinessProfileViewController.swift
@@ -52,25 +52,6 @@ class UserBusinessProfileViewController: UIViewController, UITableViewDataSource
     @IBAction func favoriteButtonTapped(_ sender: UIButton) {
         // Save the business listing as a favorite using Core Data
         CoreDataManager.shared.saveFavorite(businessListing: businessListing)
-        
-        let totalSegments = 5
-        let itemsPerSegment = 2  // or any other number you wish
-        
-        let shuffledSegments = Array(0..<totalSegments).shuffled()
-        
-        for (segmentIndex, segment) in shuffledSegments.enumerated() {
-            // Shuffle the images and take the first 'itemsPerSegment' items
-            let segmentImages = FruitsAndVeggieImages.shuffled().prefix(itemsPerSegment)
-            
-            for itemIndex in 0..<itemsPerSegment {
-                let delay = Double(segmentIndex * itemsPerSegment + itemIndex) * 0.3
-                
-                DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
-                    let fruitOrVeggie = spawnFruitOrVeggie(in: self.view, segment: segment, totalSegments: totalSegments, itemIndex: itemIndex, shuffledImages: Array(segmentImages))
-                    animateFall(of: fruitOrVeggie, in: self.view)
-                }
-            }
-        }
     }
     
     
