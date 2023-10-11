725742753e3a3ad9b0affa7339a2b989ad7511f0
Kole Ervine <koleervine@gmail.com>
2023-10-11 16:14:11 -0600

    added comments and cleared a bug with the permissions for location request

--- FarmersMarketSocialMedia/Controller/CoreLocationManager.swift
+++ FarmersMarketSocialMedia/Controller/CoreLocationManager.swift
@@ -8,48 +8,76 @@
 import Foundation
 import CoreLocation
 
-protocol CoreLocationManagerDelegate: AnyObject {
-    func didUpdateZipCode(_ zipCode: String)
-    func didFailWithError(_ error: Error)
-}
-
+// A custom location manager class to handle location requests and inform the delegate about location updates or errors.
 class CoreLocationManager: NSObject, CLLocationManagerDelegate {
     
+    // A flag to determine if permission was requested. Used to decide when to show the error to the user.
+    var didRequestPermission = false
+    
+    // The system's location manager instance.
     private var locationManager = CLLocationManager()
+    
+    // A delegate that will be notified about location updates or errors.
     weak var delegate: CoreLocationManagerDelegate?
     
+    // Initializer sets up the location manager.
     override init() {
         super.init()
         setupLocationManager()
     }
     
+    // Sets the delegate for the location manager and requests authorization for location access when in use.
     private func setupLocationManager() {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
     }
     
+    // Function to request the current location.
     func requestLocation() {
+        // Check if the location permission status is not determined.
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            didRequestPermission = true
+        }
         locationManager.requestLocation()
     }
 
+    // Delegate method called when the location manager updates locations.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Get the most recent location.
         guard let location = locations.last else { return }
 
-        // Using CLGeocoder to reverse geocode the location to get the ZIP code
+        // Using CLGeocoder to reverse geocode the location to get the ZIP code.
         let geocoder = CLGeocoder()
         geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
+            // Handle the error and inform the delegate.
             if let error = error {
                 self?.delegate?.didFailWithError(error)
                 return
             }
 
+            // If successful, get the postal code (ZIP code) and inform the delegate.
             if let placemark = placemarks?.first, let postalCode = placemark.postalCode {
                 self?.delegate?.didUpdateZipCode(postalCode)
             }
         }
     }
     
+    // Delegate method called when there's an error with location updates.
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        delegate?.didFailWithError(error)
+        // If permission was not just requested, inform the delegate about the error.
+        if !didRequestPermission {
+            delegate?.didFailWithError(error)
+        }
+        // Reset the flag after handling.
+        didRequestPermission = false
     }
+}
+
+// A protocol that defines methods for informing about location updates or errors.
+protocol CoreLocationManagerDelegate: AnyObject {
+    // Called when a new ZIP code is determined.
+    func didUpdateZipCode(_ zipCode: String)
+    
+    // Called when there's an error with location updates.
+    func didFailWithError(_ error: Error)
 }
--- FarmersMarketSocialMedia/Controller/SearchCollectionViewController.swift
+++ FarmersMarketSocialMedia/Controller/SearchCollectionViewController.swift
@@ -14,10 +14,7 @@ class SearchCollectionViewController: UIViewController {
 
     @IBOutlet weak var zipCodeSearchBar: UISearchBar!
     @IBOutlet weak var radiusButton: UIButton!
-    
     @IBOutlet weak var collectionView: UICollectionView!
-    
-
     @IBOutlet weak var backgroundImage: UIImageView!
     
     let APIFarmController = APIController()
@@ -203,28 +200,45 @@ extension SearchCollectionViewController: UICollectionViewDataSource, UICollecti
     }
 }
 
-// extension to make core location work
+// This extension allows the SearchCollectionViewController to conform to the CoreLocationManagerDelegate protocol.
+// This means it can handle location updates and errors from the CoreLocationManager.
 extension SearchCollectionViewController: CoreLocationManagerDelegate {
+    
+    // This method is called when the CoreLocationManager successfully gets a ZIP code.
     func didUpdateZipCode(_ zipCode: String) {
+        // Run UI-related updates on the main thread to ensure smooth UI performance.
         DispatchQueue.main.async {
+            // Set the found ZIP code to the search bar's text.
             self.zipCodeSearchBar.text = zipCode
+            
+            // Load business listings based on the updated ZIP code.
             self.loadBusinessListings()
         }
     }
     
+    // Displays an alert when location services are disabled or there's an error in accessing them.
     func showLocationServicesAlert() {
         let alert = UIAlertController(title: "Location Services Disabled",
-                                      message: "Please enable Location Services or manually input your ZIP code.",
+                                      message: "Please enable Location Services in Settings or manually input your ZIP code.",
                                       preferredStyle: .alert)
         
+        // Action for when the user clicks "OK", dismissing the alert.
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         
+        // Action for when the user clicks "Settings", which will take them to the app's settings in the Settings app.
+        alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url)
+            }
+        }))
+        
+        // Present the alert to the user.
         present(alert, animated: true, completion: nil)
     }
-
+    
+    // This method is called when the CoreLocationManager encounters an error.
     func didFailWithError(_ error: Error) {
-        // Handle the error
+        // Show the location services alert to inform the user.
         showLocationServicesAlert()
     }
 }
-
