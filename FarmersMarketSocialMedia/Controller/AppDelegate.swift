b40942be2d6ee0177ca694998dd99b00c9087ee1
Kole Ervine <koleervine@gmail.com>
2023-10-12 18:56:27 -0600

    dark mode disabled so as not to ruin the look of the app and cause more work

--- FarmersMarketSocialMedia/Controller/AppDelegate.swift
+++ FarmersMarketSocialMedia/Controller/AppDelegate.swift
@@ -12,9 +12,15 @@ import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
         
         // The below is needed to accessing the DB
         FirebaseApp.configure()
